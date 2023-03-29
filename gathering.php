7ab0669371a772f93f585fa9fa0f89ec05f519f5
Max Maton <max@maton.info>
2023-03-29 17:09:19 +0200

    Fix syntax

--- gathering.php
+++ gathering.php
@@ -9,7 +9,7 @@ $players = array(
 	"Wouter" => 1500
 );
 
-function match($game, $order) {
+function game($game, $order) {
 	global $players;
 	$match = new ELOMatch();
 
@@ -36,56 +36,56 @@ function match($game, $order) {
 }
 # match with array from highest to lowest
 # ? - Seven Wonders
-match("Seven Wonders", array("Matthijs", "Nicolaas", array("Wouter", "Max"), "Joris"));
+game("Seven Wonders", array("Matthijs", "Nicolaas", array("Wouter", "Max"), "Joris"));
 
 # 13-02-17 - Seven Wonders
-match("Seven Wonders", array("Joris", "Max", "Nicolaas", "Wouter", "Matthijs"));
-match("Seven Wonders", array("Wouter", "Nicolaas", "Joris", "Max"));
+game("Seven Wonders", array("Joris", "Max", "Nicolaas", "Wouter", "Matthijs"));
+game("Seven Wonders", array("Wouter", "Nicolaas", "Joris", "Max"));
 
 # 30-05-17 - Sonar
-match("Sonar", array(array("Nicolaas", "Joris"), array("Max", "Wouter")));
+game("Sonar", array(array("Nicolaas", "Joris"), array("Max", "Wouter")));
 
 # 05-05-17 - Sonar
-match("Sonar", array(array("Max", "Matthijs", "Joris"), array("Nicolaas", "Wouter")));
-match("Sonar", array(array("Nicolaas", "Wouter"), array("Max", "Matthijs", "Joris")));
+game("Sonar", array(array("Max", "Matthijs", "Joris"), array("Nicolaas", "Wouter")));
+game("Sonar", array(array("Nicolaas", "Wouter"), array("Max", "Matthijs", "Joris")));
 
 # 12/06/17 - Love letter
-match("Love Letter", array("Max", array("Joris", "Matthijs"), "Nicolaas", "Wouter"));
+game("Love Letter", array("Max", array("Joris", "Matthijs"), "Nicolaas", "Wouter"));
 
 # 19-06-17 - Sonar
-match("Sonar", array(array("Max", "Matthijs"), array("Nicolaas", "Wouter", "Joris")));
-match("Sonar", array(array("Nicolaas", "Wouter", "Joris"), array("Max", "Matthijs")));
+game("Sonar", array(array("Max", "Matthijs"), array("Nicolaas", "Wouter", "Joris")));
+game("Sonar", array(array("Nicolaas", "Wouter", "Joris"), array("Max", "Matthijs")));
 
 #?
-match("Caverna", array("Matthijs", "Nicolaas", "Wouter", "Max", "Joris"));
+game("Caverna", array("Matthijs", "Nicolaas", "Wouter", "Max", "Joris"));
 
 # 30 oct
-match("Calimala", array("Max", "Matthijs", "Joris", "Wouter", "Nicolaas"));
+game("Calimala", array("Max", "Matthijs", "Joris", "Wouter", "Nicolaas"));
 
 # 07-11-17 Evolution: Climate
-match("Evolution", array("Wouter", array("Joris", "Nicolaas")));
+game("Evolution", array("Wouter", array("Joris", "Nicolaas")));
 
 # 28-11-17 Evolution: Climate
-match("Evolution", array("Max", "Matthijs", "Wouter", "Nicolaas", "Joris"));
+game("Evolution", array("Max", "Matthijs", "Wouter", "Nicolaas", "Joris"));
 
 # 11-12-17 Evolution: Climate
-match("Evolution", array("Wouter", "Max", array("Matthijs", "Joris"), "Nicolaas"));
+game("Evolution", array("Wouter", "Max", array("Matthijs", "Joris"), "Nicolaas"));
 
 # 12-03-18 Caverna
-match("Caverna", array("Nicolaas", "Matthijs", "Joris", "Wouter", ));
+game("Caverna", array("Nicolaas", "Matthijs", "Joris", "Wouter", ));
 
 # 26-03-18 Red Dragon Inn
-match("Red Dragon Inn", array("Wouter", "Max", "Nicolaas", "Joris", "Matthijs"));
-match("Red Dragon Inn", array("Matthijs", "Nicolaas", "Max", "Wouter", "Joris"));
+game("Red Dragon Inn", array("Wouter", "Max", "Nicolaas", "Joris", "Matthijs"));
+game("Red Dragon Inn", array("Matthijs", "Nicolaas", "Max", "Wouter", "Joris"));
 
 # 14-05-18 Evolution: Climate
-match("Evolution", array("Max", "Joris", "Nicolaas", "Wouter", "Matthijs"));
+game("Evolution", array("Max", "Joris", "Nicolaas", "Wouter", "Matthijs"));
 
 # 20-05-19 Carcassonne
-match("Carcassonne", array("Nicolaas", "Joris", "Matthijs", "Wouter"));
+game("Carcassonne", array("Nicolaas", "Joris", "Matthijs", "Wouter"));
 
 # 01-07-19 Evolution: Climate
-match("Evolution", array("Matthijs", "Joris", "Wouter", "Max", "Nicolaas"));
+game("Evolution", array("Matthijs", "Joris", "Wouter", "Max", "Nicolaas"));
 
 arsort($players);
 foreach($players as $name => $elo) {
